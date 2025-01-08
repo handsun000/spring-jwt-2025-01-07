980ce4c2957dc5a04eca6d5e4d89f79f8efe86ac
son <handsun000@gmail.com>
2025-01-08 09:38:07 +0900

    work

--- src/main/java/com/ll/spring_jwt_2025_01_07/global/rq/Rq.java
+++ src/main/java/com/ll/spring_jwt_2025_01_07/global/rq/Rq.java
@@ -2,6 +2,7 @@ package com.ll.spring_jwt_2025_01_07.global.rq;
 
 import com.ll.spring_jwt_2025_01_07.domain.member.member.entity.Member;
 import com.ll.spring_jwt_2025_01_07.domain.member.member.service.MemberService;
+import com.ll.spring_jwt_2025_01_07.global.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.List;
+import java.util.Optional;
 
 // Request/Response 를 추상화한 객체
 // Request, Response, Cookie, Session 등을 다룬다.
@@ -24,9 +26,10 @@ public class Rq {
 
     // 스프링 시큐리티가 이해하는 방식으로 강제 로그인 처리
     // 임시함수
-    public void setLogin(String username) {
-        UserDetails user = new User(
-                username,
+    public void setLogin(Member member) {
+        UserDetails user = new SecurityUser(
+                member.getId(),
+                member.getUsername(),
                 "",
                 List.of()
         );
@@ -41,16 +44,28 @@ public class Rq {
     }
 
     public Member getActor() {
-        SecurityContext context = SecurityContextHolder.getContext();
+//        SecurityContext context = SecurityContextHolder.getContext();
+//
+//        Authentication authentication = context.getAuthentication();
+//
+//        if (authentication == null) return null;
+//
+//        if (authentication.getPrincipal() == null || authentication.getPrincipal() instanceof String) return null;
+//
+//        UserDetails user = (UserDetails) authentication.getPrincipal();
+//        String username = user.getUsername();
+//        return memberService.findByUsername(username).get();
 
-        Authentication authentication = context.getAuthentication();
-
-        if (authentication == null) return null;
-
-        if (authentication.getPrincipal() == null || authentication.getPrincipal() instanceof String) return null;
-
-        UserDetails user = (UserDetails) authentication.getPrincipal();
-        String username = user.getUsername();
-        return memberService.findByUsername(username).get();
+        return Optional.ofNullable(
+                        SecurityContextHolder
+                                .getContext()
+                                .getAuthentication()
+                )
+                .map(Authentication::getPrincipal)
+                .filter(principal -> principal instanceof UserDetails)
+                .map(principal -> (UserDetails) principal)
+                .map(UserDetails::getUsername)
+                .flatMap(memberService::findByUsername)
+                .orElse(null);
     }
 }
